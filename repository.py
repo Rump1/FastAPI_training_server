66c2a92c4f96bd6cb0d788629c53f37f4265fd68
Andrey Soskin <soskin_andrey@mail.ru>
2024-03-27 18:31:34 +0500

    Прописал роутеры в router.py

--- repository.py
+++ repository.py
@@ -1,5 +1,5 @@
-from database import new_session, ProvidersTable
-from schemas import ProvidersAdd
+from database import new_session, ProvidersTable, ClientsTable, CallsTable, CitiesTable
+from schemas import ProvidersAdd, ClientsAdd, CallsAdd, CitiesAdd
 from sqlalchemy import select
 
 
@@ -22,5 +22,68 @@ class ProvidersRepository:
             result = await session.execute(query)
             provider_model = result.scalars().all()
             return provider_model
+
+
+class ClientsRepository:
+    @classmethod
+    async def add_one(cls, data: ClientsAdd) -> int:
+        async with new_session() as session:
+            clients_dict = data.model_dump()
+
+            client = ClientsTable(**clients_dict)
+            session.add(client)
+            await session.flush()
+            await session.commit()
+            return client.id
+
+    @classmethod
+    async def find_all(cls):
+        async with new_session() as session:
+            query = select(ClientsTable)
+            result = await session.execute(query)
+            client_model = result.scalars().all()
+            return client_model
+
+
+class CallsRepository:
+    @classmethod
+    async def add_one(cls, data: CallsAdd) -> int:
+        async with new_session() as session:
+            calls_dict = data.model_dump()
+
+            call = CallsTable(**calls_dict)
+            session.add(call)
+            await session.flush()
+            await session.commit()
+            return call.id
+
+    @classmethod
+    async def find_all(cls):
+        async with new_session() as session:
+            query = select(CallsTable)
+            result = await session.execute(query)
+            call_model = result.scalars().all()
+            return call_model
+
+
+class CitiesRepository:
+    @classmethod
+    async def add_one(cls, data: CitiesAdd) -> int:
+        async with new_session() as session:
+            cities_dict = data.model_dump()
+
+            city = CitiesTable(**cities_dict)
+            session.add(city)
+            await session.flush()
+            await session.commit()
+            return city.id
+
+    @classmethod
+    async def find_all(cls):
+        async with new_session() as session:
+            query = select(CitiesTable)
+            result = await session.execute(query)
+            city_model = result.scalars().all()
+            return city_model
 
 
--- router.py
+++ router.py
@@ -1,8 +1,8 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends
-from repository import ProvidersRepository
-from schemas import ProvidersAdd
+from repository import ProvidersRepository, ClientsRepository, CallsRepository, CitiesRepository
+from schemas import ProvidersAdd, ClientsAdd, CallsAdd, CitiesAdd
 
 providers_router = APIRouter(
     prefix="/providers",
@@ -12,13 +12,73 @@ providers_router = APIRouter(
 
 @providers_router.post("")
 async def add_provider(
-        user: Annotated[ProvidersAdd, Depends()]
+        provider: Annotated[ProvidersAdd, Depends()]
 ):
-    provider_id = await ProvidersRepository.add_one(user)
+    provider_id = await ProvidersRepository.add_one(provider)
     return {"ok": True, "provider_id": provider_id}
 
 
 @providers_router.get("")
 async def get_providers():
-    users = await ProvidersRepository.find_all()
-    return users
+    providers = await ProvidersRepository.find_all()
+    return providers
+
+
+clients_router = APIRouter(
+    prefix="/clients",
+    tags=["Clients"]
+)
+
+
+@clients_router.post("")
+async def add_client(
+        client: Annotated[ClientsAdd, Depends()]
+):
+    client_id = await ClientsRepository.add_one(client)
+    return {"ok": True, "clients_id": client_id}
+
+
+@clients_router.get("")
+async def get_clients():
+    clients = await ClientsRepository.find_all()
+    return clients
+
+
+calls_router = APIRouter(
+    prefix="/calls",
+    tags=["Calls"]
+)
+
+
+@calls_router.post("")
+async def add_call(
+        call: Annotated[CallsAdd, Depends()]
+):
+    call_id = await CallsRepository.add_one(call)
+    return {"ok": True, "calls_id": call_id}
+
+
+@calls_router.get("")
+async def get_calls():
+    calls = await CallsRepository.find_all()
+    return calls
+
+
+cities_router = APIRouter(
+    prefix="/cities",
+    tags=["Cities"]
+)
+
+
+@cities_router.post("")
+async def add_city(
+        city: Annotated[CitiesAdd, Depends()]
+):
+    city_id = await CitiesRepository.add_one(city)
+    return {"ok": True, "ciries_id": city_id}
+
+
+@cities_router.get("")
+async def get_cities():
+    cities = await CitiesRepository.find_all()
+    return cities
