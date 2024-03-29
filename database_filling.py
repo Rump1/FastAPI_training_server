a4ed2b0871d774fa232549385bcf1664b3881824
Andrey Soskin <soskin_andrey@mail.ru>
2024-03-29 16:10:50 +0500

    Удалил лишние папки из репозитория, обновил requirements

---
+++ database_filling.py
@@ -0,0 +1,4 @@
+# Файл для заполнения БД
+from repository import ProvidersRepository
+
+#ProvidersRepository.add_one()
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from database import create_tables, delete_tables
@@ -6,10 +7,10 @@ from router import providers_router, clients_router, cities_router, calls_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await delete_tables()
-    print("База очищенна")
-    await create_tables()
-    print("База создана")
+    # await delete_tables()
+    # print("База очищенна")
+    # await create_tables()
+    # print("База создана")
     yield
     print("Выключение")
 
@@ -18,3 +19,6 @@ app.include_router(providers_router)
 app.include_router(clients_router)
 app.include_router(cities_router)
 app.include_router(calls_router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
